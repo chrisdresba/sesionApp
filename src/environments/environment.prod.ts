b0c404cc232daff6517ca4698d74dce6398c8627
Cristian Barraza <cbarraza@quo.ar>
2025-11-05 21:16:57 -0300

    edit environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAR_3YOxIaC6IKobcwfW0tF2kl8WimJ8FA",
-    authDomain: "appsesion-f7b1e.firebaseapp.com",
-    databaseURL: "https://appsesion-f7b1e-default-rtdb.firebaseio.com",
-    projectId: "appsesion-f7b1e",
-    storageBucket: "appsesion-f7b1e.appspot.com",
-    messagingSenderId: "204055205195",
-    appId: "1:204055205195:web:d56e52d49ee236acf544ce"
+  //key firebase
   }
 };
